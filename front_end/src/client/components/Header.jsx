d099d435a3adb0d6de9604932a7632edee4c5771
Richard <trananhtu6403@gmail.com>
2025-04-11 00:10:30 +0700

    Revert "Update ver1.2"
    This reverts commit 30e662aa34fd8fdcfe0e8dd8131eef53d9e4cd9a.

--- front_end/src/client/components/Header.jsx
+++ front_end/src/client/components/Header.jsx
@@ -10,14 +10,14 @@ export default function Header() {
 
   return (
     <header className="fixed top-0 left-0 w-full bg-white dark:bg-gray-900 shadow-md z-50">
-      <div className="max-w-7xl mx-auto px-4 py-4 flex">
+      <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
         
-        <div className="flex space-x-4 mr-4 ">
+        <div className="flex space-x-4">
           <button onClick={() => navigate('/search')} className="text-gray-700 dark:text-white hover:text-yellow-600">
             Tìm Kiếm
           </button>
         </div>
-        <h1 className="text-2xl font-bold mr-4">
+        <h1 className="text-2xl font-bold">
             <button onClick={()=> navigate('/home')}>
                 <span className="text-yellow-300">Betú Cinemas </span>
             </button>
